a7486f58fb591f27f7a12d27a4270aeecd079b6f
FernandoTiradosG <fernando.tiradosgonzalez.web@adaits.es>
2024-02-22 15:40:26 +0100

    Actualización 22-02-24
    Creación de About, Practicas y Skills

--- one-portfolio/src/App.js
+++ one-portfolio/src/App.js
@@ -1,12 +1,18 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Skill from "./components/Skill";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
+      <Portfolio />
+      <Skill />
       <SocialLinks />
     </div>
   );
---
+++ one-portfolio/src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white " >
+      <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full" >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className=" text-xl">Persona  versátil  y  capaz  de  simultanearvarias tareas. Con amplia experiencia enentornos laborales en los que se valora elcompromiso y la capacidad de trabajar enequipo.  Me  caracterizo  por  midinamismo,  proactividad  y  entusiasmo.Poseo  buenos  conocimientos  digitales.Busco desarrollarme profesionalmente.</p>
+      </div>
+    </div>
+  )
+};
+
+export default About;
